530aa67d9224c14db8b52792a9108aa0a9fe24dc
phungkhaccuong <cuongst93@gmail.com>
2024-04-14 09:35:25 +0700

    u

--- scripts/search_evaluation.py
+++ scripts/search_evaluation.py
@@ -72,12 +72,16 @@ def main():
     print("======ranked documents  ======")
     print(ranked_docs)
 
-    ranked_docs = [{'id': '1778112572153028802', 'text': 'I, for one, am grateful for the sophisticated market participants that keep hedging their locked tokens\n\n&gt;lets us long the best performing coins without paying exorbitant funding rates\n&gt;keep fueling the rallies as their hedge is blown up https://t.co/nWI4Rhqoar', 'created_at': '2024-04-10T17:27:27+00:00', 'username': 'HsakaTrades', 'url': 'https://x.com/HsakaTrades/status/1778112572153028802', 'quote_count': 15, 'reply_count': 81, 'retweet_count': 76, 'favorite_count': 1103},
-
+    ranked_docs = [
                     {'id': '1774213419471720547', 'text': 'RT @HsakaTrades: "wolf is this the fated last cycle?"\n\nidk, I doubt it\n\nthough if you do trade it as such, trying to extract every ounce of…', 'created_at': '2024-03-30T23:13:37+00:00', 'username': 'HsakaTrades', 'url': 'https://x.com/HsakaTrades/status/1774213419471720547', 'quote_count': 13, 'reply_count': 104, 'retweet_count': 120, 'favorite_count': 2483},
                    {'id': '1774040239721381969', 'text': 'RT @HsakaTrades: gm fellow unsophisticated market participants \n\nmemecoins volume dominance will simply continue rising until \n\nsome meme b…', 'created_at': '2024-03-30T11:45:27+00:00', 'username': 'HsakaTrades', 'url': 'https://x.com/HsakaTrades/status/1774040239721381969', 'quote_count': 26, 'reply_count': 171, 'retweet_count': 161, 'favorite_count': 2107},
                    {'id': '1774045741532553354', 'text': 'the VCs whinging about the explosion of interest in memecoins are directly responsible for it\n\nwith their incessant predatory tokenomics low float high fdv shenanigans \n\nthey bit the hand that fed them, now the same hand has dropped them off at the shelter, and taken in a more friendly dog', 'created_at': '2024-03-30T12:07:19+00:00', 'username': 'HsakaTrades', 'url': 'https://x.com/HsakaTrades/status/1774045741532553354', 'quote_count': 74, 'reply_count': 224, 'retweet_count': 419, 'favorite_count': 2496},
                    {'id': '1774014990304895093', 'text': "CT avoid making statements about permanent changes in market dynamics based on a few months of activity challenge [impossible]\n\nThis one's on top, then that one's on top, and on and on, the wheel simply continues spinning. https://t.co/zKeTYeKz5E", 'created_at': '2024-03-30T10:05:07+00:00', 'username': 'HsakaTrades', 'url': 'https://x.com/HsakaTrades/status/1774014990304895093', 'quote_count': 2, 'reply_count': 74, 'retweet_count': 62, 'favorite_count': 928},
+        {'id': '1778112572153028802',
+         'text': 'I, for one, am grateful for the sophisticated market participants that keep hedging their locked tokens\n\n&gt;lets us long the best performing coins without paying exorbitant funding rates\n&gt;keep fueling the rallies as their hedge is blown up https://t.co/nWI4Rhqoar',
+         'created_at': '2024-04-10T17:27:27+00:00', 'username': 'HsakaTrades',
+         'url': 'https://x.com/HsakaTrades/status/1778112572153028802', 'quote_count': 15, 'reply_count': 81,
+         'retweet_count': 76, 'favorite_count': 1103}
                    ]
 
     # note this is the llm score, skipped integrity check and batch age score
